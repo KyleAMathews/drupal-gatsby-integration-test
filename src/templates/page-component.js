a1d9de984e52634786e62da31bc2935a24733585
Kyle Mathews <mathews.kyle@gmail.com>
2021-10-13 17:04:27 -0700

    add missing component

---
+++ src/templates/page-component.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+export default function PageComponent(props) {
+  return (
+    <div>
+      <div id="title">{props.data.nodePage.title}</div>
+      <div>{props.data.nodePage.body.processed}</div>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query ($id: String) {
+    nodePage(id: { eq: $id }) {
+      id
+      title
+      body {
+        processed
+      }
+    }
+  }
+`
